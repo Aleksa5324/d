5d2f9f5666bd30ce2b19fd6c2aa75264cdbffd18
raDreamer <rodion.dolhii@gmail.com>
2017-06-26 22:05:56 +0300

    Added update interval for chart data

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<title>Голосование</title>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="scripts.js"></script>
   </head>
   <body>
--- scripts.js
+++ scripts.js
@@ -1,27 +1,36 @@
-//Создаем диаграмму GoogleChart как написано в документации Google
+document.onload = function() {
+  //Создаем диаграмму GoogleChart как написано в документации Google
 
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-	  
-      function drawChart() {
-		var jsonData = $.ajax({
-          url: "data.php",
-          dataType: "json",
-          async: false
-        }).responseText;
-		  
-		  	  
-		  
-       	var data = new google.visualization.DataTable(jsonData); //  а в примере Google используется массив
-			
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
 
-        var options = {
-          title: 'Нужны ли выборы в Украине?',
-		  //legend: 'none',
-		  //pieSliceText: 'label',
-		  is3D: true,
-        };
+  var chart = new google.visualization.PieChart($('#piechart_3d'));
+  var UPDATE_INTERVAL = 1000;
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
+  function getData() {
+    var jsonData = $.ajax({
+      url: "data.php",
+      dataType: "json",
+      async: false
+    });
+
+    return jsonData.responseText;
+  }
+
+  function updateView() {
+    drawChart();
+  }
+
+  function drawChart() {
+    var data = new google.visualization.DataTable(getData()); //  а в примере Google используется массив
+    var options = {
+      title: 'Нужны ли выборы в Украине?',
+      //legend: 'none',
+      //pieSliceText: 'label',
+      is3D: true
+    };
+    chart.draw(data, options);
+  }
+
+  setInterval(updateView, UPDATE_INTERVAL);
+};
