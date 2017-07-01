85be5c467385a37a1756a0d392491a56881513dd
taknado72 <taknado72@ukr.net>
2017-07-01 14:31:22 +0300

    внесены изменения

--- scripts.js
+++ scripts.js
@@ -1,49 +1,50 @@
-widnow.onload = function() {
-  // Создаем диаграмму GoogleChart как написано в документации Google
-  google.charts.load("current", {packages:["corechart"]});
-  google.charts.setOnLoadCallback(drawChart);
+// Создаем диаграмму GoogleChart как написано в документации Google
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(startInterval);
 
-  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-  var UPDATE_INTERVAL = 1000;
+var chart;
+var UPDATE_INTERVAL = 1000;
 
-  function getData() {
-    var jsonData = $.ajax({
-      url: "data.php",
-      dataType: "json",
-      async: false
-    }).done(function(response) {
-      var array = JSON.parse(response.responseText);
-      array.forEach(function(choise) {
-        $('#result').append(
-          '<tr>' +
-            '<td>' + choise[0] + '</td>' +
-            '<td>' + choise[1] + '</td>' +
-          '</tr>'
-        )
-      });
-    }).fail(function(error) {
-      throw Error('Failed get data from server: ' + error.responseText);
-    });
+function getData() {
+	var jsonData = $.ajax({
+	  url: "data.php",
+	  dataType: "json",
+	  async: false
+	}).done(function(response) {
+	  $('#result #choises').remove();
+	  response.forEach(function(choise) {
+		$('#result').append(
+		  '<tr id="choises"' +
+			'<td>' + choise[0] + '</td>' +
+			'<td>' + choise[1] + '</td>' +
+		  '</tr>'
+		)
+	  });
+	}).fail(function(error) {
+	  throw Error('Failed get data from server: ' + error.responseText);
+	});
 
-    // [["openvox-gsm-yes","5"],["openvox-gsm-no","7"]]
+	// [["openvox-gsm-yes","5"],["openvox-gsm-no","7"]]
 
-    return jsonData.responseText;
-  }
+	return jsonData.responseText;
+}
 
-  function updateView() {
-    drawChart();
-  }
+function updateView() {
+	drawChart();
+}
 
-  function drawChart() {
-    var data = new google.visualization.DataTable(getData()); // а в примере Google используется массив
-    var options = {
-      title: 'Нужны ли выборы в Украине?',
-      //legend: 'none',
-      //pieSliceText: 'label',
-      is3D: true
-    };
-    chart.draw(data, options);
-  }
+function drawChart() {
+	var data = new google.visualization.DataTable(getData()); // а в примере Google используется массив
+	var options = {
+	  title: 'Нужны ли выборы в Украине?',
+	  //legend: 'none',
+	  //pieSliceText: 'label',
+	  is3D: true
+	};
+	chart.draw(data, options);
+}
 
-  setInterval(updateView, UPDATE_INTERVAL);
-};
+function startInterval() {
+	chart = new google.visualization.PieChart(document.getElementById('piechart_3d'))
+	setInterval(updateView, UPDATE_INTERVAL);
+}
