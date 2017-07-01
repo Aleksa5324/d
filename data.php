2d063ecd11631c7ae3562fb21aa443754c3f046e
raDreamer <rodion.dolhii@gmail.com>
2017-07-01 13:37:05 +0300

    fixed table draw on main page

--- data.php
+++ data.php
@@ -1,35 +1,24 @@
 <?php
-
-
-
-// подключаемся к серверу 
-$db = mysqli_connect("127.0.0.1", "root", "", "cdr") or die("Ошибка " . mysqli_error($db));
-mysqli_set_charset($db, "utf8");
-
+  // подключаемся к базе 
+  $db = mysqli_connect("127.0.0.1", "root", "", "cdr") or die("Ошибка " . mysqli_error($db));
+  mysqli_set_charset($db, "utf8");
 
 function getDataString(){
-	global $db;
-	$query = "SELECT `dcontext`,COUNT(`dst`) AS dst
-FROM `cdr` WHERE `dst`='gsm0'
-GROUP BY `dcontext`
-UNION
-SELECT `dcontext`,COUNT(`dst`) AS dst
-FROM `cdr` WHERE `dst`='gsm2'
-GROUP BY `dcontext`;";
+  global $db;
+  $query = "SELECT `dcontext`,COUNT(`dst`) AS dst
+    FROM `cdr` WHERE `dst`='gsm0'
+    GROUP BY `dcontext`
+    UNION
+    SELECT `dcontext`,COUNT(`dst`) AS dst
+    FROM `cdr` WHERE `dst`='gsm2'
+    GROUP BY `dcontext`;";
 
-	$res = mysqli_query($db, $query);
-	$data = '{"cols": [';
-	$data .= '{"label":"Да","type":"string"},';
-	$data .= '{"label":"Нет","type":"number"}';
-	$data .= '],"rows": [';
+  $res = mysqli_query($db, $query);
+  $data = mysqli_fetch_all($res);
 
-	while($row = mysqli_fetch_assoc($res)){
-		$data .= '{"c":[{"v":"' . $row['dcontext'] . '"},{"v":' . $row['dst'] . '}]},';
-	}
-	$data = rtrim($data, ',');
-	$data .= ']}';
-	return $data;
+  return json_encode($data);
 }
+
 echo getDataString();
 
 // закрываем подключение
--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-document.onload = function() {
-  //Создаем диаграмму GoogleChart как написано в документации Google
-
+widnow.onload = function() {
+  // Создаем диаграмму GoogleChart как написано в документации Google
   google.charts.load("current", {packages:["corechart"]});
   google.charts.setOnLoadCallback(drawChart);
 
-  var chart = new google.visualization.PieChart($('#piechart_3d'));
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
   var UPDATE_INTERVAL = 1000;
 
   function getData() {
@@ -12,7 +11,21 @@ document.onload = function() {
       url: "data.php",
       dataType: "json",
       async: false
+    }).done(function(response) {
+      var array = JSON.parse(response.responseText);
+      array.forEach(function(choise) {
+        $('#result').append(
+          '<tr>' +
+            '<td>' + choise[0] + '</td>' +
+            '<td>' + choise[1] + '</td>' +
+          '</tr>'
+        )
+      });
+    }).fail(function(error) {
+      throw Error('Failed get data from server: ' + error.responseText);
     });
+
+    // [["openvox-gsm-yes","5"],["openvox-gsm-no","7"]]
 
     return jsonData.responseText;
   }
@@ -22,7 +35,7 @@ document.onload = function() {
   }
 
   function drawChart() {
-    var data = new google.visualization.DataTable(getData()); //  а в примере Google используется массив
+    var data = new google.visualization.DataTable(getData()); // а в примере Google используется массив
     var options = {
       title: 'Нужны ли выборы в Украине?',
       //legend: 'none',
