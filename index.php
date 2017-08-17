4cb83ac67e847e056445e4311d9050a45f043abf
taknado72 <taknado72@ukr.net>
2017-08-17 16:34:31 +0300

    добавлено

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+include_once 'connect.php';
+
+
 //вывод сайта для зарегистрированных пользователей
-if (!isset($_SESSION['USER_LOGIN_IN']) && $_SESSION['USER_LOGIN_IN'] =0 ) {
+if (!isset($_SESSION['USER_LOGIN_IN']) or $_SESSION['USER_LOGIN_IN'] =0 ) {
 	header ("Location:page/signin.php");
 	exit();
 } elseif (isset($_SESSION['USER_LOGIN_IN']) && $_SESSION['USER_LOGIN_IN'] =1){
